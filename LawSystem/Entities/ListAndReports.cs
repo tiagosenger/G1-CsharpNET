e840a2a8db45b447123d9db582977b4daa1b13b4
LobinhoTroll <gabrielteles931@gmail.com>
2023-12-04 16:27:20 -0300

    busca  estado civil, ordem alfabética e profissão

--- LawSystem/Entities/ListAndReports.cs
+++ LawSystem/Entities/ListAndReports.cs
@@ -1,5 +1,3 @@
-using System.Diagnostics.Metrics;
-
 class ListAndReports{
     public void advogadosFaixaIdade(Advogados advogados)
     {
@@ -16,9 +14,12 @@ class ListAndReports{
         }
         else
         {
+            Console.WriteLine("Lista de advogados com essa faixa de idade: ");
+            int counter = 1;
             foreach (var x in correspondencias)
             {
-                Console.WriteLine(x);
+                Console.WriteLine(counter+". "+x);
+                counter += 1;
             }
         }
         
@@ -38,12 +39,65 @@ class ListAndReports{
         }
         else
         {
+            Console.WriteLine("Lista de clientes com esta faixa de idade: ");
+            int counter = 1;
             foreach (var x in correspondencias)
             {
-                Console.WriteLine(x);
+                Console.WriteLine(counter+". "+x);
+                counter += 1;
             }
         }
     }
-    
+    public void clientesEstadocivilInformado(Cliente cliente)
+    {
+        var correspondencias = cliente.Any(x => x.EstadoCivil == estadocivil).Tolist();
+        if (correspondencias == null)
+        {
+            Console.WriteLine("Não há clientes com este estado civil.");
+        }
+        else
+        {
+            Console.WriteLine("Lista de clientes com o estado civil selecionado: ");
+            int counter = 1;
+            foreach (var x in correspondencias)
+            {
+                Console.WriteLine(counter+". "+x);
+                counter += 1;
+            }
+        }
+    }
+    public void clientesOrdemAlfabetica(Cliente cliente){
+        var listaOrdenada =  new List<string>();
+        listaOrdenada = cliente.Lista;
+        listaOrdenada.Sort();
+
+        Console.WriteLine("Lista de clientes em ordem alfabética: ");
+        int counter = 1;
+        foreach (var x in listaOrdenada)
+        {
+            Console.WriteLine(counter+". "+x);
+            counter += 1;
+        }
+    }
+    public void clientesProfissaoSelecionada(Cliente cliente)
+    {
+        Console.WriteLine("Informe a profissão a ser critério de filtro: ");
+        string profissao = Console.ReadLine()!.ToLower();
+        var correspondencias = cliente.Any(x => x.Profissao.ToLower() == profissao).ToList();
+        if (correspondencias == null)
+        {
+            Console.WriteLine("Não há clientes com essa profissão.");
+        }
+        else
+        {
+            Console.WriteLine("Lista de clientes com a profissão selecionada: ");
+            int counter = 1;
+            foreach (var x in correspondencias)
+            {
+                Console.WriteLine(counter+". "+x);
+                counter += 1;
+            }
+        }
+    }
 
 }
